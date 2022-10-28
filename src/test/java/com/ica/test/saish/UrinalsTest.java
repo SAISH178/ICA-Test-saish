72113c0ca5bd42abf950d731c56ac56d0f5feb8c
SAISH1789 <45946634+SAISH1789@users.noreply.github.com>
2022-10-28 02:01:03 -0700

    testcase3 success

--- src/test/java/com/ica/test/saish/UrinalsTest.java
+++ src/test/java/com/ica/test/saish/UrinalsTest.java
@@ -25,7 +25,7 @@ public class UrinalsTest {
 	@Test
 	void test3() {
 		Urinals urinals = new Urinals();
-		String str = "10000";
+		String str = "00000";
 		int res = urinals.countUrinals(str);
 		assertEquals(3, res);
 	}
